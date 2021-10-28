694761644bdf48c8e60ea17646a4e032559bfe2b
unknown <hapyjeevicool@gmail.com>
2021-10-28 22:15:17 +0530

    files updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+
+import CountUp from 'react-countup';
+
 import './App.css';
 
 function App() {
+  const [data,setData] = useState({})
+
+  const url = "https://api.rootnet.in/covid19-in/stats/latest";
+  
+  useEffect(() => {
+    fetch(url).then(response => response.json()).then(result => {
+      setData(result.data.summary)
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1> Infected : <CountUp start={0} end={data.confirmedCasesIndian} duration={5}></CountUp></h1>
+       <h1> Discharged : <CountUp start={0} end={data.discharged} duration={5}></CountUp> </h1>
+       <h1> Demise : <CountUp start={0} end={data.deaths} duration={5}></CountUp> </h1>
       </header>
     </div>
   );
